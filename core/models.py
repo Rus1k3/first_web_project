8435833a520ac38f7cf4b71ed0b7a4b827a5b6fc
Rusik <143873668+Rus1k3@users.noreply.github.com>
2026-05-18 01:14:37 +0300

    Task 5 ready

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailMessage(models.Model):
+    sender = models.CharField(max_length=255)
+    recipient = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    received_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
